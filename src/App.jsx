845991d70dd2a842151220474679f669d4c893fe
Amos Gloria <amoskesegloria.com>
2026-01-23 19:40:04 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/navbar/page';
+
+function App() {
+
+  return (
+    <>
+      <div className='w-full h-[170px] bg-[#131B31]'>
+       <Navbar/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar/page.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Home from '/src/assets/images/home.svg';
+import About from '/src/assets/images/about.svg';
+import Skills from '/src/assets/images/skills.svg';
+import Project from '/src/assets/images/project.svg';
+import Experience from '/src/assets/images/experience.svg';
+import Contact from '/src/assets/images/contact.svg'
+
+export default function Navbar() {
+  return (
+    <div>
+        <section>
+            <div className='flex text-[14px] gap-8 text-white font'>
+                <div className='flex gap-2'>
+                    <img src={Home}/>
+                    <p>Home</p>
+                </div>
+               <div className='flex gap-2'>
+                <img src={About}/>
+                <p>About</p>
+               </div>
+                <div className='flex gap-2'>
+                    <img src={Skills}/>
+                    <p>Skills</p>
+                </div>
+                <div className='flex gap-2'>
+                    <img src={Project}/>
+                    <p>Projects</p>
+                </div>
+                <div className='flex gap-2'>
+                    <img src={Experience}/>
+                    <p>Experience</p>
+                </div>
+                <div className='flex gap-2'>
+                    <img src={Contact}/>
+                    <p>Contact</p>
+                </div>
+            </div>
+        </section>
+    </div>
+  )
+}
